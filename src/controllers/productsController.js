c81a92af1d3d8611df4c0b55b8c8c2a4002bfe0d
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-03-28 15:06:51 +0200

    Added skeleton for productsController.

---
+++ src/controllers/productsController.js
@@ -0,0 +1,9 @@
+exports.getAll = (req, res) => {};
+
+exports.get = (req, res) => {};
+
+exports.create = (req, res) => {};
+
+exports.update = (req, res) => {};
+
+exports.delete = (req, res) => {};
