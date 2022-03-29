b070ad9e6743d4bc8b100d907f1c9c31d4438b96
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-03-29 10:11:31 +0200

    Added CRUD for products.

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,9 +1,68 @@
-exports.getAll = (req, res) => {};
+const Product = require('../models/productModel');
 
-exports.get = (req, res) => {};
+exports.getAll = async (req, res, next) => {
+  try {
+    const result = await Product.find().exec();
+    res.status(200).json(result);
+  } catch (err) {
+    next(err, req, res);
+  }
+};
 
-exports.create = (req, res) => {};
+exports.get = async (req, res, next) => {
+  try {
+    const productId = req.params.id;
+    const result = await Product.findById(productId).exec();
+    if (!result)
+      return res.status(404).json({
+        status: 404,
+        message: `Product with id ${productId} not found`
+      });
+    res.status(200).json(result);
+  } catch (err) {
+    next(err, res, req);
+  }
+};
 
-exports.update = (req, res) => {};
+exports.create = async (req, res, next) => {
+  try {
+    const result = await Product.create(req.body);
+    console.log(result);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err, req, res);
+  }
+};
 
-exports.delete = (req, res) => {};
+exports.update = async (req, res, next) => {
+  try {
+    const productId = req.params.id;
+    const changes = { ...req.body, modified: Date.now() };
+    const result = await Product.findOneAndUpdate({ _id: productId }, changes, {
+      new: true
+    });
+    if (!result)
+      return res.status(404).json({
+        status: 404,
+        message: `Product with id ${productId} not found`
+      });
+    res.status(200).json(result);
+  } catch (err) {
+    next(err, req, res);
+  }
+};
+
+exports.delete = async (req, res, next) => {
+  try {
+    const productId = req.params.id;
+    const result = await Product.findByIdAndDelete(productId).exec();
+    if (!result)
+      return res.status(404).json({
+        status: 404,
+        message: `Product with id ${productId} not found`
+      });
+    res.status(200).json(result);
+  } catch (err) {
+    next(err, req, res);
+  }
+};
