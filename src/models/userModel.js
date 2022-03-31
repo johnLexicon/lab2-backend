95dd30eccaef562379bd4eb25d8908e0e2b853db
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-03-31 16:15:36 +0200

    Using default timestamps instead.

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,23 +2,24 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const { isEmail } = require('validator');
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: {
-    type: String,
-    required: [true, 'Please enter an email'],
-    unique: true,
-    validate: [isEmail, 'Please enter a valid email']
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: {
+      type: String,
+      required: [true, 'Please enter an email'],
+      unique: true,
+      validate: [isEmail, 'Please enter a valid email']
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, 'Password must be at least 6 characters long']
+    }
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: [6, 'Password must be at least 6 characters long']
-  },
-  created: { type: Date, default: Date.now },
-  modified: { type: Date, default: Date.now }
-});
+  { timestamps: true }
+);
 
 // Fires before the doc is saved to the db (use function expression instead of arrow function for "this")
 userSchema.pre('save', async function (next) {
