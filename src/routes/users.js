1621e00ae273c60efbedfe925e46d0df61b9e5cb
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-04-03 13:41:07 +0200

    Usage of routeguard middleware in users.

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 const userRouter = require('express').Router();
 const usersController = require('../controllers/usersController');
+const { requireAuth } = require('../middleware/authMiddleware');
 
-userRouter.get('/', usersController.get);
+userRouter.get('/', requireAuth, usersController.get);
 
 module.exports = userRouter;
